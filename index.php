1e3657ea42804adba38302ead7e82f4fc087fc8c
lagrimka <lapaille1983@mail.ru>
2017-01-25 13:36:55 +0300

    Сохранение index

--- index.php
+++ index.php
@@ -5,6 +5,12 @@ require 'app.php';
 
 // работа с логикой
 $content = "Текущая страница $page";
+if ($page == "page1"){
+    $template = "views/page1.php";
+} else {
+    $template = "views/default.php";
+}
+    
 
 // отображение результата
 include 'views/layout.php';
